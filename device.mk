c9ce23dea1de3696debf2fab042153643a61c85c
Faridul Islam <arnobislam0012@gmail.com>
2020-07-03 21:38:59 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/realme/RMX1921/RMX1921-vendor.mk)
+$(call inherit-product-if-exists, vendor/realme/RMX1901/RMX1901-vendor.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
