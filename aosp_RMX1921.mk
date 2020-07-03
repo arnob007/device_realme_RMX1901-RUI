a04c95ce6c4ea0c5bf3beaec443b3f90cdf7d275
Faridul Islam <arnobislam0012@gmail.com>
2020-07-03 21:36:33 +0600

    Update aosp_RMX1921.mk

--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -16,16 +16,16 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Realme
-PRODUCT_DEVICE := RMX1921
+PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_RMX1921
-PRODUCT_MODEL := Realme XT
+PRODUCT_NAME := aosp_RMX1901
+PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200605.001 6392402 release-keys" \
-    PRODUCT_NAME="RMX1921"
+    PRODUCT_NAME="RMX1901"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys
