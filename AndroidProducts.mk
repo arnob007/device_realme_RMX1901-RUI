b048af7b2cc6f6feebe588fee1d8b9503362ec71
Faridul Islam <arnobislam0012@gmail.com>
2020-07-03 21:32:15 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1921.mk
+    $(LOCAL_DIR)/aim_RMX1921.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1921-userdebug \
-    aosp_RMX1921-eng
+    aim_RMX1921-userdebug \
+    aim_RMX1921-eng
